35470109dabbade2c47b864a8ef940d27689933a
ikuma-e <139757329+ikuma-e@users.noreply.github.com>
2023-08-04 14:12:02 +0900

    Issue6修正２

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plans)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
@@ -25,7 +25,7 @@ class CalendarsController < ApplicationController
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
-    @week_days = Date.today.wday
+    @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
@@ -35,12 +35,12 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = @week_days
+      wday_num = Date.today.wday + x
       if wday_num >= 7
-        wday_num = wday_num -7
+        wday_num = wday_num - 7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => @week_days }
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] }
       @week_days.push(days)
     end
 
